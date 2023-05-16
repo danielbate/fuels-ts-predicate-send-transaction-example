bf473a3a62d06ede8bdd9f4d26eb4563b3c2a3fb
danielbate <djbate23@gmail.com>
2023-05-16 11:35:51 +0100

    chore: exemplar usage of predicate send transaction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,51 +4,39 @@ import logo from './logo.svg';
 import './App.css';
 import { Address, CoinStatus, JsonFlatAbi, NativeAssetId, Predicate, Provider, ScriptTransactionRequest, Wallet, WalletUnlocked, bn } from 'fuels';
 import { BackendAbi__factory } from './types';
+import type { CoinQuantityLike } from 'fuels';
+import { ValidationInput } from "./types/factories/BackendAbi__factory";
 
 function App() {
-  // Connect to testnet beta-3
+  // setup beta 3 provider
   const provider = new Provider('https://beta-3.fuel.network/graphql');
+  // setup wallet
+  const wallet = Wallet.fromPrivateKey(process.env.WALLET_PRIVATE_KEY as string, provider);
+  // instantiate predicate
   const predicate = BackendAbi__factory.createInstance(provider);
-
+  // validation to satisfy predicate
+  const predicateValidation: ValidationInput = {
+    has_account: true,
+    total_complete: 100,
+  };
   
   useEffect(() => {
     const connectPredicate = async () => {
-      // Reference test code: https://github.com/FuelLabs/fuels-ts/blob/eda13d72c32f72652a34f926c4b9cf42ac36556c/packages/predicate/src/predicate.test.ts#L96
-      predicate.setData({has_account: true, total_complete: 100});
-      // TODO add priv key
-      const wallet = Wallet.fromPrivateKey("0x..", provider);
+
+      predicate.setData(predicateValidation);
+
+      const coinQuantity: CoinQuantityLike = { amount: 1_000_000, assetId: NativeAssetId};
+
+      const resources = await predicate.getResourcesToSpend([coinQuantity]);
 
       const request = new ScriptTransactionRequest({ gasLimit: 1000, gasPrice: 1});
 
-      // A coin is added
-      request.addResources([{
-        // id: '0x01' gives error "Invalid b256 (argument="b256", value="0x01", code=INVALID_ARGUMENT..""
-        // this input gives error "Invalid struct UtxoId. Field "outputIndex" not present. (argument="UtxoId", value"
-        id: '0x0000000000000000000000000000000000000000000000000000000000000001',
-        assetId: NativeAssetId,
-        amount: bn(1),
-        owner: predicate.address,
-        status: CoinStatus.Unspent,
-        maturity: 1,
-        blockCreated: bn(1),
-      }]);
-      console.log(request);
-      
-      /*
-      This is one of the steps executed in predicate.sendTransaction(request)
-      seems to be where the issue is
+      request.addResources([resources[0]]);
 
-      Error: Invalid struct UtxoId. Field "outputIndex" not present. (argument="UtxoId", value={"transactionId":"0x0000000000000000000000000000000000000000000000000000000000000001"}, code=INVALID_ARGUMENT, version=0.41.0)
-      */
-      const a1 = request.toTransactionBytes();
-      console.log(a1);
+      const response = await predicate.sendTransaction(request);
 
-      /*
-      Error: Invalid struct UtxoId. Field "outputIndex" not present. (argument="UtxoId", value={"transactionId":"0x0000000000000000000000000000000000000000000000000000000000000001"}, code=INVALID_ARGUMENT, version=0.41.0)
-      */
-      predicate.sendTransaction(request);
+      console.log('response', response);
     }
-
     connectPredicate()
       .catch(console.error);
   }, []);
